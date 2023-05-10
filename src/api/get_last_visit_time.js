dddf5a20e1757b5a882e42c914b67965e8747637
Oleksandr <olexander11441144@gmail.com>
2023-05-10 22:00:51 +0300

    update logic, cleanup

--- src/api/get_last_visit_time.js
+++ src/api/get_last_visit_time.js
@@ -6,51 +6,35 @@ export default function getLastVisitTime(timestamp) {
   const now = Date.now();
   const todayStart = new Date().setHours(0, 0, 0, 0);
   const yesterdayStart = todayStart - 24 * 60 * 60 * 1000;
+  const visitDate = new Date(timestamp);
 
   if (timestamp >= todayStart && timestamp <= now) {
-    const visitTime = new Date(timestamp);
     return (
-      "був у мережі о " +
-      visitTime.toLocaleTimeString("uk-UA", {
+      "last visited at " +
+      //maybe replace to navigator.language
+      visitDate.toLocaleTimeString("uk-UA", {
         hour: "2-digit",
         minute: "2-digit",
       })
     );
   } else if (timestamp >= yesterdayStart && timestamp < todayStart) {
-    const visitTime = new Date(timestamp);
     return (
-      "був у мережі вчора о " +
-      visitTime.toLocaleTimeString("uk-UA", {
+      "last visited yesterday at " +
+      //maybe replace to navigator.language
+      visitDate.toLocaleTimeString("uk-UA", {
         hour: "2-digit",
         minute: "2-digit",
       })
     );
   } else {
-    const visitDate = new Date(timestamp);
     return (
-      "був у мережі " +
-      visitDate.getDate() +
-      " " +
-      getMonthName(visitDate.getMonth())
+      "last visited on " +
+      //maybe replace to navigator.language
+      visitDate.toLocaleDateString("uk-UA", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })
     );
   }
-}
-
-export function getMonthName(monthIndex) {
-  const monthNames = {
-    0: "січня",
-    1: "лютого",
-    2: "березня",
-    3: "квітня",
-    4: "травня",
-    5: "червня",
-    6: "липня",
-    7: "серпня",
-    8: "вересня",
-    9: "жовтня",
-    10: "листопада",
-    11: "грудня",
-  };
-
-  return monthNames[monthIndex];
 }
