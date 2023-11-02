9bdcba89fbe365002fad8f89d1d45469421012f6
Oleksandr <olexander11441144@gmail.com>
2023-11-02 17:21:44 +0200

    SAMA-47 added urlify function in utils

---
+++ src/utils/urlify.js
@@ -0,0 +1,20 @@
+import globalConstants from "../_helpers/constants";
+
+export function urlify(inputText) {
+  const matches = inputText.match(globalConstants.linksRegExp) || [];
+  const parts = inputText.split(globalConstants.linksRegExp);
+
+  return parts.reduce((result, part, i) => {
+    result.push(part);
+
+    if (i < matches.length) {
+      result.push(
+        <a href={matches[i]} target="_blank" key={i}>
+          {matches[i]}
+        </a>
+      );
+    }
+
+    return result;
+  }, []);
+}
