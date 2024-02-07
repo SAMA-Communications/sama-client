d9c9c411f3fca5fa9ffae6a1a0d5012a017bccac
Oleksandr <olexander11441144@gmail.com>
2024-02-07 13:20:49 +0200

    SAMA-79 remove scroll-bar__outer-container

--- src/newcomponents/_helpers/CustomScrollBar.js
+++ src/newcomponents/_helpers/CustomScrollBar.js
@@ -3,8 +3,7 @@ import Scrollbars from "react-custom-scrollbars-2";
 export default function CustomScrollBar({ children, customStyle, customId }) {
   return (
     <Scrollbars
-      className="scroll-bar__outer-container"
-      autoHide
+      // autoHide
       autoHideTimeout={400}
       autoHideDuration={400}
       style={customStyle}
