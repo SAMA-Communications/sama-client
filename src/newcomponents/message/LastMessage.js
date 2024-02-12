26e9d3bde680d3a59bb7a0e7a12862a40027cb84
Oleksandr <olexander11441144@gmail.com>
2024-02-12 17:01:30 +0200

    SAMA-79 revert  last message commit

--- src/newcomponents/message/LastMessage.js
+++ src/newcomponents/message/LastMessage.js
@@ -9,14 +9,13 @@ export default function LastMessage({ message, count, userId }) {
 
   const { attachments, body } = message;
   const lastAtt = attachments?.slice(-1)[0];
-  const isFile = lastAtt && lastAtt.type === "file";
 
   return (
     <>
       <div className="content-bottom__last-message">
-        {lastAtt && !isFile ? <LastMessageMedia attachment={lastAtt} /> : null}
+        {lastAtt ? <LastMessageMedia attachment={lastAtt} /> : null}
         <p className="last-message__text">
-          {body?.length ? body : isFile ? getFileType(lastAtt.file_name) : ""}
+          {body?.length ? body : getFileType(lastAtt.file_name)}
         </p>
       </div>
       {count > 0 ? (
