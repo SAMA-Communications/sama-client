5f06f7a52277ba271cd9bf8e6f80bd862d5cb973
Oleksandr <olexander11441144@gmail.com>
2024-02-12 17:16:14 +0200

    SAMA-79 add last message text view

--- src/newcomponents/message/LastMessage.js
+++ src/newcomponents/message/LastMessage.js
@@ -10,13 +10,19 @@ export default function LastMessage({ message, count, userId }) {
   const { attachments, body } = message;
   const lastAtt = attachments?.slice(-1)[0];
 
+  const lastMessageText = (text, att) => {
+    if (!text && !att) {
+      return "";
+    }
+    const fileType = att && getFileType(att.name);
+    return text || fileType || "";
+  };
+
   return (
     <>
       <div className="content-bottom__last-message">
         {lastAtt ? <LastMessageMedia attachment={lastAtt} /> : null}
-        <p className="last-message__text">
-          {body?.length ? body : getFileType(lastAtt.file_name)}
-        </p>
+        <p className="last-message__text">{lastMessageText(body, lastAtt)}</p>
       </div>
       {count > 0 ? (
         <div className="content-bottom__indicator">{count}</div>
