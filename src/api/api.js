8875ca670c1874e51b8f92d425b1dde793060fde
AntonSelezev <anton@connectycube.com>
2024-02-14 15:42:44 +0200

    update appication_id

--- src/api/api.js
+++ src/api/api.js
@@ -158,7 +158,7 @@ class Api {
               login: data.ulogin,
               password: data.pass,
               deviceId: getBrowserFingerprint(true),
-              application_id: 4
+              application_id: 6783
             },
         id: getUniqueId("userLogin"),
       },
