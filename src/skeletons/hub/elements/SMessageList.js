1c10b8d7c3678c7af56882a1194ae3f8d7e704c9
Oleksandr <olexander11441144@gmail.com>
2024-03-04 14:42:11 +0200

    SK-79 update skeleton message list

--- src/skeletons/hub/elements/SMessageList.js
+++ src/skeletons/hub/elements/SMessageList.js
@@ -2,13 +2,18 @@ import SChatMessage from "./SChatMessage";
 
 export default function SMessageList() {
   return (
-    <>
-      <SChatMessage />
-      <SChatMessage />
-      <SChatMessage />
-      <SChatMessage />
-      <SChatMessage />
-      <SChatMessage />
-    </>
+    <div className="infinite-scroll-component__outerdiv">
+      <div
+        className="infinite-scroll-component "
+        style={{ height: "auto", overflow: "auto" }}
+      >
+        <SChatMessage />
+        <SChatMessage />
+        <SChatMessage />
+        <SChatMessage />
+        <SChatMessage />
+        <SChatMessage />
+      </div>
+    </div>
   );
 }
