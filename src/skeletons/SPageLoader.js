f3a34ee76b33264a51c00db7fe7ae313e1031dda
Oleksandr <olexander11441144@gmail.com>
2024-03-05 11:17:57 +0200

    SK-79 update spageloader to oval

--- src/skeletons/SPageLoader.js
+++ src/skeletons/SPageLoader.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { FallingLines } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
 
 export default function SPageLoader() {
   return (
     <div className="page-loader fcc">
-      <FallingLines
-        color="var(--color-accent-dark)"
+      <Oval
+        height={100}
         width={100}
+        color="var(--color-accent-dark)"
+        wrapperStyle={{}}
+        wrapperClass={{}}
         visible={true}
-        ariaLabel="falling-circles-loading"
+        ariaLabel="oval-loading"
+        secondaryColor="var(--color-accent-dark)"
+        strokeWidth={2}
+        strokeWidthSecondary={3}
       />
     </div>
   );
