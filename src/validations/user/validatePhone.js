fcf8bd1909c1cb2cc36eaa538d7438619f2c76ff
Oleksandr <olexander11441144@gmail.com>
2024-05-15 14:07:07 +0300

    SK-172 updated RegExp for mobile phone

--- src/validations/user/validatePhone.js
+++ src/validations/user/validatePhone.js
@@ -1,7 +1,7 @@
 import showCustomAlert from "@utils/show_alert";
 
 export default function validatePhone(phone) {
-  if (phone && !/^[0-9]{3,15}$/.test(phone)) {
+  if (phone && !/^(\+\d{2,14}|\d{3,15})$/.test(phone)) {
     showCustomAlert(
       "The phone number should be 3 to 15 digits in length.",
       "warning"
