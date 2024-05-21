f207e69b7a35f9adba9445e9cc47070433e0863c
Oleksandr <olexander11441144@gmail.com>
2024-05-21 11:42:06 +0300

    SK-99 fixed chat indicator on create chat firstly

--- src/store/values/Conversations.js
+++ src/store/values/Conversations.js
@@ -110,10 +110,10 @@ export const conversations = createSlice({
         }
       }
 
-      updateParams.messagesIds = [...mids, msg._id];
+      console.log(conv.unread_messages_count);
       if (countOfNewMessages) {
         updateParams.unread_messages_count =
-          conv.unread_messages_count + countOfNewMessages;
+          (conv.unread_messages_count || 0) + countOfNewMessages;
       }
 
       conversationsAdapter.upsertOne(state, updateParams);
