2ac37ff3318da782c7fe7480eee564743336cc98
Oleksandr <olexander11441144@gmail.com>
2024-06-24 13:09:49 +0300

    SK-197 implemented search chat by chatName

--- src/components/search/SearchBlock.js
+++ src/components/search/SearchBlock.js
@@ -1,8 +1,12 @@
+import ChatList from "@components/search/lists/ChatList";
 import CustomScrollBar from "@components/_helpers/CustomScrollBar";
 import OvalLoader from "@components/_helpers/OvalLoader";
 import SearchedUser from "@components/search/elements/SearchedUser";
+import conversationService from "@services/conversationsService";
 import usersService from "@services/usersService";
+import { selectConversationsEntities } from "@store/values/Conversations";
 import { useEffect, useState, useTransition } from "react";
+import { useSelector } from "react-redux";
 
 import "@styles/search/SearchBlock.css";
 
@@ -17,12 +21,17 @@ export default function SearchBlock({
   isSelectUserToArray = false,
   isClearInputText = false,
   isPreviewUserProfile = false,
+  isSearchOnlyUsers = false,
 }) {
   const viewProperty = (v) => ({ display: v ? "block" : "none" });
 
+  const conversations = useSelector(selectConversationsEntities);
+
   const [isPending, startTransition] = useTransition();
   const [searchedUsers, setSearchedUsers] = useState([]);
+  const [searchedChats, setSearchedChats] = useState([]);
   const [isUserSearched, setIsUserSearched] = useState(null);
+  const [isChatSearched, setIsChatSearched] = useState(null);
 
   useEffect(() => {
     if (!searchText) {
@@ -38,12 +47,24 @@ export default function SearchBlock({
       if (text?.length > 1) {
         const users = await usersService.search({
           login: text,
-          limit: 30,
+          limit: 5,
         });
-
         setSearchedUsers(users);
         setIsUserSearched(
           users.length ? null : "We couldn't find the specified user."
+        );
+
+        if (isSearchOnlyUsers) {
+          return;
+        }
+
+        const conversations = await conversationService.search({
+          name: text,
+          limit: 10,
+        });
+        setSearchedChats(conversations);
+        setIsChatSearched(
+          conversations.length ? null : "We couldn't find the specified chat."
         );
       }
     });
@@ -53,8 +74,11 @@ export default function SearchBlock({
     <div className="search__container fcc" style={viewProperty(searchText)}>
       {isPending ? (
         <OvalLoader width={80} height={80} />
-      ) : searchedUsers.length ? (
+      ) : searchText?.length < 2 ? null : (
         <CustomScrollBar customStyle={{ width: "400px" }}>
+          {isSearchOnlyUsers ? null : (
+            <div className="search__list-title">Users</div>
+          )}
           {searchedUsers.map((u) => (
             <SearchedUser
               key={u._id}
@@ -76,9 +100,16 @@ export default function SearchBlock({
               isPreviewUserProfile={isPreviewUserProfile}
             />
           ))}
+          <p className="search__text">{isUserSearched}</p>
+          {isSearchOnlyUsers ? null : (
+            <ChatList
+              conversations={searchedChats
+                .map((obj) => conversations[obj._id])
+                .filter((el) => !!el)}
+              isChatSearched={isChatSearched}
+            />
+          )}
         </CustomScrollBar>
-      ) : (
-        <p className="search__text">{isUserSearched}</p>
       )}
     </div>
   );
---
+++ src/components/search/lists/ChatList.js
@@ -0,0 +1,50 @@
+import ConversationItem from "@components/hub/elements/ConversationItem";
+import getUserFullName from "@utils/user/get_user_full_name";
+import navigateTo from "@utils/navigation/navigate_to";
+import { getConverastionById } from "@store/values/Conversations";
+import { selectCurrentUser } from "@store/values/CurrentUser";
+import { selectParticipantsEntities } from "@store/values/Participants";
+import { setSelectedConversation } from "@store/values/SelectedConversation";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function ChatList({
+  conversations,
+  isShowTitle = true,
+  isChatSearched,
+}) {
+  const dispatch = useDispatch();
+
+  const participants = useSelector(selectParticipantsEntities);
+  const currentUser = useSelector(selectCurrentUser);
+  const selectedConversation = useSelector(getConverastionById);
+  const activeConv = selectedConversation?._id;
+
+  return (
+    <>
+      {isShowTitle ? <div className="search__list-title">Chats</div> : null}
+      {conversations.map((obj) => (
+        <ConversationItem
+          key={obj._id}
+          isSelected={activeConv === obj._id}
+          onClickFunc={() => {
+            dispatch(setSelectedConversation({ id: obj._id }));
+            navigateTo(`/#${obj._id}`);
+          }}
+          chatName={
+            obj.name ||
+            getUserFullName(
+              participants[
+                obj[
+                  obj.owner_id === currentUser._id ? "opponent_id" : "owner_id"
+                ]
+              ] || {}
+            )
+          }
+          chatObject={obj}
+          currentUserId={currentUser._id}
+        />
+      ))}
+      <p className="search__text">{isChatSearched}</p>
+    </>
+  );
+}
