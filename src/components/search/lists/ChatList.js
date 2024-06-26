1f65e7b73d70a116ba7d6f5cd5f994ad8f6e04e4
Oleksandr <olexander11441144@gmail.com>
2024-06-26 14:37:12 +0300

    SK-197 hide title of chats

--- src/components/search/lists/ChatList.js
+++ src/components/search/lists/ChatList.js
@@ -7,7 +7,9 @@ export default function ChatList({
 }) {
   return (
     <>
-      {isShowTitle ? <div className="search__list-title">Chats</div> : null}
+      {isShowTitle && conversations.length ? (
+        <div className="search__list-title">Chats</div>
+      ) : null}
       <ConversationItemList conversations={conversations} />
       <p className="search__text">{isChatSearched}</p>
     </>
