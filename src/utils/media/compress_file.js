b32f04652308894b56193e09f22c49f1675f3403
Oleksandr <olexander11441144@gmail.com>
2024-07-22 09:41:35 +0300

    SK-225 change quality for compression

--- src/utils/media/compress_file.js
+++ src/utils/media/compress_file.js
@@ -1,7 +1,7 @@
 import * as imageConversion from "image-conversion";
 
 export default async function compressFile(file) {
-  const compressedfile = await imageConversion.compress(file, 0.9);
+  const compressedfile = await imageConversion.compress(file, 0.3);
   const formData = new FormData();
   formData.append("file", compressedfile, file.name);
 
