60d2a897d04e7d18257efa4c8e72f8713179ac16
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2024-12-10 13:48:40 +0200

    SK-278 updated .env.example

--- src/utils/navigation/build_http_url.js
+++ src/utils/navigation/build_http_url.js
@@ -1,5 +1,5 @@
 export default function buildHttpUrl() {
-  return `http${process.env.IS_SSL ? "s" : ""}://${
+  return `http${process.env.DEVELOPMENT ? "s" : ""}://${
     process.env.REACT_APP_SOCKET_CONNECT.split("//")[1]
   }`;
 }
