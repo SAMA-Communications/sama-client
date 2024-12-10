9d431a929d4ac1b5479c651eafcdf862f0e3bdad
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2024-12-10 14:10:11 +0200

    SK-278 minor fix

--- src/utils/navigation/build_http_url.js
+++ src/utils/navigation/build_http_url.js
@@ -1,5 +1,5 @@
 export default function buildHttpUrl() {
-  return `http${process.env.DEVELOPMENT ? "s" : ""}://${
+  return `http${process.env.DEVELOPMENT ? "" : "s"}://${
     process.env.REACT_APP_SOCKET_CONNECT.split("//")[1]
   }`;
 }
