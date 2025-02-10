82de7db35d2c39036796e158d5d19574de739896
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2025-02-10 19:56:55 +0200

    removed log

--- src/services/encryptionService.js
+++ src/services/encryptionService.js
@@ -140,10 +140,6 @@ class EncryptionService {
   }
 
   async #buildAndEncodePickleKey(data, userId, deviceId) {
-    console.log("buildAndEncodePickleKey");
-    console.log("crypto", crypto);
-    console.log("subtle", crypto?.subtle);
-
     if (!crypto?.subtle) {
       return undefined;
     }
@@ -192,8 +188,6 @@ class EncryptionService {
     let isNewAccount = false;
     if (encAuthAccount && pickleKey) {
       try {
-        console.log(encAuthAccount, pickleKey);
-
         this.#account = Account.from_pickle(
           encAuthAccount,
           decodeBase64(pickleKey)
