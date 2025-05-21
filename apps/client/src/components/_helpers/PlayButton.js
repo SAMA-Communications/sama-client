60371dc687780e064785b4649c5f8429370c019c
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2025-05-22 00:43:12 +0300

    PlayButton z-index fixed

--- apps/client/src/components/_helpers/PlayButton.js
+++ apps/client/src/components/_helpers/PlayButton.js
@@ -1,7 +1,7 @@
 export default function PlayButton({ onClickFunc }) {
   return (
     <span
-      className="absolute top-1/2 left-1/2 flex justify-center items-center w-[45px] h-[45px] rounded-full bg-(--color-bg-light-50) text-gray-700 z-10 transform -translate-x-1/2 -translate-y-1/2"
+      className="absolute top-1/2 left-1/2 flex justify-center items-center w-[45px] h-[45px] rounded-full bg-(--color-bg-light-50) text-gray-700 transform -translate-x-1/2 -translate-y-1/2"
       onClick={onClickFunc}
     >
       ▶
